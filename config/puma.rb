89d1b890e2d3ee36b395a203661d854bf1303a91
Ken Ding <ken.ding@mail.com>
2017-04-20 03:29:43 +0930

    remove redundent folder git control

--- config/puma.rb
+++ config/puma.rb
@@ -1,12 +1,12 @@
-# http://stackoverflow.com/questions/20801734/error-while-starting-puma-server-with-workers
-# workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
 preload_app!
 
 rackup      DefaultRackup
-port        ENV['PORT']     || 3000
+bind        'unix:///tmp/nginx.socket'
+# port       3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
